c18aa67f4deeee7cb89d73ca296912613a6736a7
kakusia <wtecho25@gmail.com>
2025-03-21 19:13:05 +0900

    study1

---
+++ scanner1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class scanner1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter a string");
+        String str = sc.nextLine();
+        System.out.println(str);
+    }
+}
---
+++ study1.java
@@ -0,0 +1,27 @@
+public class study1 {
+    public static void main(String[] args) {
+        boolean a1 = true;
+        char ch = 'A';
+        byte b1 = 127;
+        short a2 = 12341;
+        int i = 123;
+        int oct = 0100;
+        int hex = 0x100;
+        float f1 = 100f;
+        long l = 10123124L;
+        int sum = b1 + a2;
+        String s1 = "hellox";
+        System.out.println(a1);
+        System.out.println(ch);
+        System.out.println(b1);
+        System.out.println(a2);
+        System.out.println(i);
+        System.out.println(oct);
+        System.out.println(hex);
+        System.out.println(s1);
+        System.out.println(f1);
+        System.out.println(l);
+        System.out.println(""+sum);
+        System.out.printf("age:%d",14);
+    }
+}
