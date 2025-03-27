189b3a5fbb82ace6c4878a58976ba952738d9952
kakusi <wtecho331@gmail.com>
2025-03-27 18:54:15 +0900

    03/27끝

---
+++ src/StringTest.java
@@ -0,0 +1,8 @@
+public class StringTest {
+    public static void main(String[] args) {
+        String str1 = "Hello World";
+        System.out.println(str1);
+        str1 = "e213";
+        System.out.println(str1);
+    }
+}
